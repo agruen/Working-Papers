7446a15450fad42be1e83356e0a81a8f5fbade1e
Andrew Gruen <ag@orangechair.org>
2024-03-08 13:49:23 -0500

    Update docker-compose-upgrade.sh
    fix colors and improve viewing of docker logs for the test run of the new version

--- Paperless-ngx/docker-compose-upgrade.sh
+++ Paperless-ngx/docker-compose-upgrade.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-# This is a simple bash script to do a few things related to upgrading [Paperless-ngx](https://docs.paperless-ngx.com) if you're using the official docker containers.  It should help you 1) Backup your data 2) Stop your current containers 3) Download the latest version 4) Test-run that version 5) Run the new version in daemon mode (if all went well) and 6) purge any docker containers not currently in use (so you don't keep a huge number of old versions of things lying around).
-
 # Define a file to capture docker compose output
 DOCKER_OUTPUT_FILE="docker_output.log"
 
 # Define colors for printing headers
 print_green() {
-    echo "\033[1;92m$1\033[0m"
+    echo -e "\033[1;92m$1\033[0m"
 }
 
 print_red() {
-    echo "\033[0;31m$1\033[0m"
+    echo -e "\033[0;31m$1\033[0m"
 }
 
 # 1) Run the backup script
 print_green "Starting the backup process..."
-./backup.sh
+docker exec paperless-webserver-1 document_exporter ../export
 if [ $? -ne 0 ]; then
     print_red "Backup process failed. Exiting..."
     exit 1
@@ -39,19 +37,18 @@ if [ $? -ne 0 ]; then
 fi
 
 # 4) Restart the container, outputting to a file
-print_green "Restarting the container and capturing output for the next 30 seconds..."
+print_green "Restarting the container and capturing output for the next 45 seconds..."
 sudo docker compose up > "$DOCKER_OUTPUT_FILE" 2>&1 &
 DOCKER_PID=$!
 
-# Wait for 30 seconds to capture some initial logs
-sleep 30
-
-# Display the captured output
-print_green "Displaying initial docker compose up output:"
-cat "$DOCKER_OUTPUT_FILE"
+# Show the output of Docker for 45 seconds, via the log file
+tail -f "$DOCKER_OUTPUT_FILE" &
+TAIL_PID=$!
+sleep 45
+kill $TAIL_PID
 
 # 5) Wait for user input to confirm if things are working
-print_green "Please review the output above. Enter 'y' if everything is working correctly, or any other key to stop."
+print_green "Please review the output above and check that Paperless-ngx is running as you expect. Enter 'y' if everything is working correctly, or any other key to stop."
 read -p "Is everything working correctly? (y/n): " answer
 if [ "$answer" != "y" ]; then
     print_red "User indicated a problem. Stopping the container in 30 seconds..."
